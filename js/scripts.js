8bf056f759d50b235b475c9fcd2f17bf54786634
David McCaslin <davidmccaslin94@gmail.com>
2020-08-14 10:13:43 -0700

    updated user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 // Business logic
 
+
 // User interface logic
 
 $(document).ready(function() {
-  console.log("This is working.");
+  // console.log("This is working.");
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    const answer1 = $("input#question1").val();
+    const answer2 = $("input#question2").val();
+    const answer3 = $("input#question3").val();
+  })
 });
